5f850e2436bc1dc2717441d038d666d45f106007
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-10 19:24:52 -0700

    Update app.js
    I created the start button's function.

---
+++ app.js
@@ -0,0 +1,38 @@
+let mainDiv = document.querySelector(".stopwatch");
+let miliSec = document.querySelector("#mili-sec");
+let sec = document.querySelector("#sec");
+let minutes = document.querySelector("#min");
+let hour = document.querySelector("#hour");
+let startBtn = document.querySelector("#start");
+let resetBtn = document.querySelector("#reset");
+let stopBtn = document.querySelector(".stop");
+let getTimeBtn = document.querySelector(".get-time");
+let clearTimeBtn = document.querySelector(".clear-time");
+let miliSecCount = 0;
+let secCount = 1;
+let minCount = 1;
+let hourCount = 1;
+
+const startBtnFunc = () => {
+    setInterval(() => {
+        miliSec.innerText = miliSecCount++;
+        if (miliSecCount === 200) {
+            miliSecCount = 0;
+            secCount < 10 ? sec.innerText = `0${secCount++}` : sec.innerText = secCount++;
+
+            if (secCount === 60) {
+                secCount = 0;
+                minCount < 10 ? minutes.innerText = `0${minCount++}` : minutes.innerText = minCount++;
+
+                if (minCount === 60) {
+                    minCount = 0;
+                    hourCount < 10 ? hour.innerText = `0${hourCount++}` : hour.innerText = hourCount++;
+                }
+            }
+        }
+    }, 0);
+};
+
+startBtn.addEventListener("click" , () => {
+    startBtnFunc();
+});
